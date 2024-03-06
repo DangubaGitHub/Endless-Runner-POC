625d3e316cfdd63f594b72bf6dca87454d2a9b38
DangubaGitHub <101139413+DangubaGitHub@users.noreply.github.com>
2024-03-05 20:01:56 -0800

    Commit 015
    Made system to destroy game elements after hitting wall following player

--- Endless Runner POC/Assets/Scripts/Level/DestroyOverTime.cs
+++ Endless Runner POC/Assets/Scripts/Level/DestroyOverTime.cs
@@ -2,26 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyOnConntact : MonoBehaviour
+public class DestroyOverTime : MonoBehaviour
 {
-
+    public float timeTillDestroyed;
+    
+    
     void Start()
     {
         
     }
 
+
     void Update()
     {
+        timeTillDestroyed -= Time.deltaTime;
         
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Destroyer"))
+        if (timeTillDestroyed <= 0)
         {
             Destroy(gameObject);
-            
-            Debug.Log("conntact");
         }
     }
 }

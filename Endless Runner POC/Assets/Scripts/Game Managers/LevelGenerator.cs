269ad7ac2a4c5acd0ed707d21b43ea8dcb8011a1
DangubaGitHub <101139413+DangubaGitHub@users.noreply.github.com>
2024-03-13 20:58:33 -0700

    Commit 018
    finished endless easy level spawning

--- Endless Runner POC/Assets/Scripts/Game Managers/LevelGenerator.cs
+++ Endless Runner POC/Assets/Scripts/Game Managers/LevelGenerator.cs
@@ -1,42 +1,53 @@
-using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Mathematics;
+//using Unity.Mathematics;
 using UnityEngine;
+//using Random = UnityEngine.Random;
+
+//using Random = System.Random;
 
 public class LevelGenerator : MonoBehaviour
 {
+    private const float DistanceBetweenPlayerAndLevelSpawn = 75f;
+
+    [SerializeField] private GameObject player;
+    
     [SerializeField] private Transform levelPart_Start;
-    [SerializeField] private Transform levelPart_1;
-    [SerializeField] private Transform levelPart_2;
-    [SerializeField] private Transform levelPart_3;
-    [SerializeField] private Transform levelPart_4;
-    [SerializeField] private Transform levelPart_5;
+    [SerializeField] private List<Transform> easyLevelParts;
+    [SerializeField] private List<Transform> mediumLevelParts;
+    [SerializeField] private List<Transform> hardLevelParts;
+    [SerializeField] private List<Transform> insaneLevelParts;
 
     private Vector3 lastEndPosition;
+    private Transform chosenLevelPart;
+    private Transform lastLevelPartTransform;
+    private Transform levelPartTransform;
 
     private void Awake()
     {
         lastEndPosition = levelPart_Start.Find("End Position").position;
-        SpawnLevelParts();
-        SpawnLevelParts();
-        SpawnLevelParts();
-    }
-
-    private void SpawnLevelParts()
-    {
-        Transform lastLevelPartTransform = SpawnLevelParts(lastEndPosition);
-        lastEndPosition = lastLevelPartTransform.Find("End Position").position;
-    }
-
-    private Transform SpawnLevelParts(Vector3 spawnPosition)
-    {
-        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
-        return levelPartTransform;
     }
 
     void Update()
     {
+        if (Vector3.Distance(player.transform.position, lastEndPosition) < DistanceBetweenPlayerAndLevelSpawn)
+        {
+            SpawnLevelParts();
+        }
+    }
+
+    private void SpawnLevelParts()
+    {
+        chosenLevelPart = easyLevelParts[Random.Range(0, easyLevelParts.Count)];
         
+        lastLevelPartTransform = SpawnLevelParts(chosenLevelPart, lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("End Position").position;
+    }
+
+    private Transform SpawnLevelParts(Transform levelPart,Vector3 spawnPosition)
+    {
+        levelPartTransform = Instantiate(levelPart, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
     }
 }
